352acfb042817477cf1ef2052ba3c9f9f2dc960e
Agung Rakhmat Gumilar <agungrg17@gmail.com>
2022-07-14 22:50:30 +0700

    code deployment

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth';
 
 // TODO: replace with your own config
 const firebaseConfig = {
-    apiKey: "AIzaSyBGqktQBjz5Ow4bmkDO2v2epilGwIrPF40",
+    //apiKey: "AIzaSyBGqktQBjz5Ow4bmkDO2v2epilGwIrPF40",
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: "auth-react-a3477.firebaseapp.com",
     projectId: "auth-react-a3477",
     storageBucket: "auth-react-a3477.appspot.com",
     messagingSenderId: "841109424677",
     appId: "1:841109424677:web:d59c818268158556124dcc"
-    // apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    
     // authDomain: "mood-meter-13966.firebaseapp.com",
     // projectId: "mood-meter-13966",
     // storageBucket: "mood-meter-13966.appspot.com",
